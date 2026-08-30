bf9378bb333ae10a519c8c072699570ead14cef5
Sona V S <sonavs22102006@gmail.com>
2026-08-30 19:39:20 +0530

    Fix CORS policy to allow Vercel

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ initDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || '*' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
